ed4aa955d864ec98c3bd3d236eff26b5a88124dc
Konst <junecat@gmail.com>
2022-03-27 20:35:56 +0300

    Simple SQLite example in folder SimpleSqlite

---
+++ SimpleSqlite/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+
+namespace SimpleSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> taleList = new List<string>();
+            using (var connection = new SqliteConnection(@"Data Source=hello.sqlite")) {
+                SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
+                connection.Open();
+                const string sql_query = "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';";
+                var command = new SqliteCommand(sql_query, connection);
+
+                using (var reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        var name = reader.GetString(0);
+                        taleList.Add(name);
+                        Console.WriteLine($"{name}");
+                    }
+                }
+            }
+        }
+    }
+}
